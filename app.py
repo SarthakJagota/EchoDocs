e12b3b366bf4d6566fbef94f1c79ae8ae5f4eafb
Sarthak Jagota <sarthakjagota34@gmail.com>
2025-07-25 00:17:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -46,6 +46,6 @@ def ask():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-if _name_ == "_main_":
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
-        
+
